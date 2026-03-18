1275382fce381c641a385664ff2b36e7ceb14f63
toshiKen <162226507+NeKoNeKo13@users.noreply.github.com>
2026-03-18 11:54:16 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = createRoot(document.getElementById("root")!);
+root.render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
